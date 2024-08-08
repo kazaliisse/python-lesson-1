b5fd8a33123d52008650fd28189ec186de77c66d
noor <noorhafowbare@gmail.com>
2024-08-08 10:46:34 +0300

    initial commit

--- lesson-1.py
+++ lesson-1.py
@@ -1,2 +1,7 @@
 greeting  = 'hello world'
 print(greeting)
+your_name = input("please enter your name")
+print(your_name)
+num1 = input("enter a number")
+num2 = input("enter a number")
+print(int(num1) - int(num2))
